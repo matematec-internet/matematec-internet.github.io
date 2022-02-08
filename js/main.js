5eb6b2c846ca956f602c8ffc64d6e50e7f2d1ba4
matematec-internet <alfonso_otero@yahoo.com>
2022-02-08 07:43:26 -0600

    Agregando horarios y artículos

--- js/main.js
+++ js/main.js
@@ -2,25 +2,25 @@ window.addEventListener("scroll", function(){var header = document.querySelector
 
 function inicio(){
 	document.getElementById('inicio').scrollIntoView();
-	menu.classList.toggle('active_menu');
+	menu.classList.remove('active_menu');
 }
 
 function servicios(){
 	document.getElementById('servicios').scrollIntoView();
-	menu.classList.toggle('active_menu');
+	menu.classList.remove('active_menu');
 }
 
 function venta(){
 	document.getElementById('venta').scrollIntoView();
-	menu.classList.toggle('active_menu');
+	menu.classList.remove('active_menu');
 }
 
 function horarios(){
 	document.getElementById('horarios').scrollIntoView();
-	menu.classList.toggle('active_menu');
+	menu.classList.remove('active_menu');
 }
 
 function impresiones(){
 	document.getElementById('impresiones').scrollIntoView();
-	menu.classList.toggle('active_menu');
+	menu.classList.remove('active_menu');
 }
